5671429b3ab10e1232c3d4a35696e444e8400288
Chris Huang <chrishuang128@gmail.com>
2020-09-01 07:32:11 -0700

    initialized server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const PORT = 1300;
+
+let app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/../public/dist'));
+app.use(express.urlencoded({ extended: false }));
+
+// app.get('/app', (req, res)=>{
+// 	res.status(200);
+// })
+
+
+
+
+app.listen(PORT, ()=>{
+	console.log(`Server listening on ${PORT}`);
+})
+
+
+
+
