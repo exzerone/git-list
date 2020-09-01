d07337b75ebb9108bb2907398ae50f15607440a3
Chris Huang <chrishuang128@gmail.com>
2020-09-01 14:55:37 -0700

    implement pagination and page state

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,33 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import CardList from './components/CardList.jsx'
+import CardList from './components/CardList.jsx';
+import Pagination from './components/Pagination.jsx';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+	text-align: center;
+`;
 
 function App() {
 	let [users, setUsers] = useState([]);
+	let [page, setPage] = useState(1);
+	let [isLoading, setIsLoading] = useState(false);
 
-	useEffect(()=> {
+	useEffect(() => {
+		setIsLoading(true);
 		Axios.get('https://api.github.com/users', {
 			params: {
-				since: 0
-			}
+				since: 30 * page - 30,
+			},
 		})
-		.then(data => {
-			if (data) {
-				setUsers(data.data);
-			}
+		.then((response) => {
+			const users = response.data;
+			setUsers(users);
+			setIsLoading(false);
 		})
-		.catch(error => console.log(error))
-	}, [])
+		.catch((error) => console.log(error));
+	}, [page]);
+
+	const handleClick = function (page) {
+		setPage(page);
+	};
 
 	return (
 		<div>
-			<h1 className="title">
-					Github Users
-			</h1>
-			<CardList list={users}/>
+			<Title className="title">Github Users</Title>
+			<h2>Number of Users per page: {users.length}</h2>
+			{isLoading ? (
+				<div className="loading">Loading...</div>
+			) : (
+				<Pagination totalPages={20} handleClick={handleClick} page={page} />
+			)}
+			<CardList list={users} />
 		</div>
-	)
+	);
 }
 
 export default App;
