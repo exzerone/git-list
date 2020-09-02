a6a9b77350b6c54098ca522e225e232f3ca2f03d
Chris Huang <chrishuang128@gmail.com>
2020-09-02 15:40:11 -0700

    add testing for card

---
+++ test/Card.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from '../src/components/Card.jsx';
+const data = require('../data.json');
+
+let wrapper = shallow(<Card user={data[0]}></Card>);
+
+describe("Card", ()=> {
+	it('should render without crashing', ()=>{
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('renders the user title', ()=>{
+		expect(wrapper.find('.user_title').text()).toEqual(`username: ${data[0]['login']}`);
+	});
+});
+
