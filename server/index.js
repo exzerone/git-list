8c2b74955d87de049eecf86447c23e24c44dcee3
Chris Huang <chrishuang128@gmail.com>
2020-09-03 10:09:45 -0700

    remove unnecessary middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const morgan = require('morgan');
 const PORT = 1300;
 
 let app = express();
 
-app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../public/dist'));
 app.use(express.urlencoded({ extended: false }));
 
